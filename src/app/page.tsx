db2a4262a09a14d1fef70ca6245fc9e9ec1c04ca
kenneth <kenethjhon99@gmail.com>
2025-12-23 11:00:24 -0600

    first try

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import SearchIcon from "@/components/icons/searchIcon";
+const Home = () => {
+  return (
+    <>
+      <form className="flex flex-wrap items-center gap-4 rounded-xl bg-blue-900 p-2 ">
+        <span className="min-w-[30px]">
+          <SearchIcon
+            className="fill-blue-500"
+            width={30}
+          />
+        </span>
+        <input
+          type="text"
+          placeholder="Search Github Username..."
+          className=" h-14 flex-1 rounded-lg bg-transparent p-3 text-white placeholder-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button className="rounded-lg bg-blue-500 px-4 py-4 font-bold text-white ">
+          Search
+        </button>
+      </form>
+      <article>todo lo de la pagina</article>
+    </>
+  );
+};
+export default Home;
